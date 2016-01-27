c9f602d1484d869858e66fc61f8a01b4a788cd4d
Novik <novik65@gmail.com>
2016-01-27 10:44:54 +0300

    fix twitter

--- lib/social_shares/twitter.rb
+++ lib/social_shares/twitter.rb
@@ -1,6 +1,6 @@
 module SocialShares
   class Twitter < Base
-    URL = 'http://cdn.api.twitter.com/1/urls/count.json'
+    URL = 'http://opensharecount.com/count.json'
 
     def shares!
       response = get(URL, params: { url: checked_url })
