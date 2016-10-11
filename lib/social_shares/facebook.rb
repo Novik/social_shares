958bc34ae851224128201bfaec4b8ca9ea4fe141
Novik <novik65@gmail.com>
2016-10-11 17:31:39 +0300

    add diagnostic

--- lib/social_shares/facebook.rb
+++ lib/social_shares/facebook.rb
@@ -1,12 +1,26 @@
 module SocialShares
   class Facebook < Base
-    URL = 'http://graph.facebook.com/v2.7/'
+    URL = 'http://graph.facebook.com/v2.8/'
 
     def shares!
       response = get(URL, params: {
         id: checked_url,
         fields: 'share'
       })
+
+      RestClient::Resource.new(URL, timeout: TIMEOUT, open_timeout: OPEN_TIMEOUT).get(params: {
+        id: checked_url,
+        fields: 'share'
+      }){ |response, request, result, &block|
+        case response.code
+          when 200
+            response
+          else
+            puts response
+            raise
+        end
+      }
+
       json_response = JSON.parse(response)
 
       if json_response['share']
